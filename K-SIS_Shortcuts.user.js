20d1506e495e17b0f5a9da70d511c011fb21bae4
Dan Berghoff <djberghoff0@gmail.com>
2023-12-02 19:01:28 -0500

    Update K-SIS_Shortcuts.user.js

--- K-SIS_Shortcuts.user.js
+++ K-SIS_Shortcuts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         K-SIS Shorcuts
 // @namespace    http://tampermonkey.net/
-// @version      0.4.5
+// @version      0.4.6
 // @description  Various shortcuts to make using K-SIS less annoying
 // @author       Dan Berghoff
 // @updateURL    https://github.com/dberghoff/K-SIS-Shortcuts/blob/main/K-SIS_Shortcuts.user.js
